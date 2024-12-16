e2cb8048dd858c93f61b3f747812e720fb9e9cef
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2024-12-16 23:50:09 +0200

    fix main

--- main.cpp
+++ main.cpp
@@ -1,110 +1,193 @@
-#include <cinttypes>
-#include <windows.h>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
 
-static bool quit = false;
+constexpr int WindowWidth = 1280;
+constexpr int WindowHeight = WindowWidth * (9.0f / 16.f);
 
-LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+const float vertices[] = {
+        -1.0f, -1.0f,
+        3.0f, -1.0f,
+        -1.0f,  3.0f
+};
 
-static int frameWidth{};
-static int frameHeight{};
-static uint32_t* frameData;
-static BITMAPINFO bitmapInfo;
-static HBITMAP bitmap;
-static HDC frameDeviceContext;
+std::string loadShaderSource(const std::string& fileName) {
+    std::string path = "C:\\Users\\grigo\\Repos\\software-renderer\\shader\\";
+    path += fileName;
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR cmdLine, int cmdShow) {
-    const char window_class_name[] = "My Window Class";
-    static WNDCLASS window_class   = {0};
-    window_class.lpfnWndProc       = windowProc;
-    window_class.hInstance         = instance;
-    window_class.lpszClassName     = window_class_name;
-    RegisterClass(&window_class);
+    std::ifstream in(path);
+    std::stringstream fileData;
+    fileData << in.rdbuf();
 
-    bitmapInfo.bmiHeader.biSize        = sizeof(bitmapInfo.bmiHeader);
-    bitmapInfo.bmiHeader.biPlanes      = 1;
-    bitmapInfo.bmiHeader.biBitCount    = 32;
-    bitmapInfo.bmiHeader.biCompression = BI_RGB;
-    frameDeviceContext                 = CreateCompatibleDC(0);
+    return fileData.str();
+}
 
-    static HWND window_handle;
-    window_handle = CreateWindow(
-            window_class_name,
-            reinterpret_cast<LPCSTR>(L"Drawing Pixels"),
-            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-            640,
-            300,
-            1280,
-            720,
-            nullptr,
-            nullptr,
-            instance,
-            nullptr
-    );
-    if (window_handle == nullptr) {
+GLuint compileShader(GLenum shaderType, const std::string& source) {
+    auto shader = glCreateShader(shaderType);
+    const char* sourceCStr = source.c_str();
+    glShaderSource(shader, 1, &sourceCStr, nullptr);
+    glCompileShader(shader);
+
+    int compileStatus;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
+    if (compileStatus != GL_TRUE) {
+        char log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, log);
+        std::cerr << "Shader compilation failed:\n" << log << "\n";
+    }
+
+    return shader;
+}
+
+unsigned int createShaderProgram(const std::string& vertexPath, const std::string& fragmentPath) {
+    std::string vertexSource = loadShaderSource(vertexPath);
+    std::string fragmentSource = loadShaderSource(fragmentPath);
+    if (vertexSource.empty() || fragmentSource.empty()) {
+        return 0;
+    }
+
+    auto vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    auto fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    auto program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    int linkStatus;
+    glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+    if (linkStatus != GL_TRUE) {
+        char log[512];
+        glGetProgramInfoLog(program, 512, nullptr, log);
+        std::cerr << "Program linking failed:\n" << log << "\n";
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
+unsigned int createTexture(int width, int height) {
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    unsigned char* data = new unsigned char[width * height * 3];
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            int index = (y * width + x) * 3;
+            data[index + 0] = (int)(((float)x / (float)width) * 255);
+            data[index + 1] = (int)(((float)y / (float)height) * 255);
+            data[index + 2] = 50;
+        }
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    delete[] data;
+    return texture;
+}
+
+unsigned int createPBO(int width, int height) {
+    unsigned int pbo;
+    glGenBuffers(1, &pbo);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * 3, nullptr, GL_STREAM_DRAW);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    return pbo;
+}
+
+void updateTexture(unsigned int texture, unsigned int pbo, int width, int height, const void* data) {
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+    void* buffer = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+    if (buffer) {
+        std::memcpy(buffer, data, width * height * 3);
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    while (!quit) {
-        static MSG message = {0};
-        while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE)) { DispatchMessage(&message); }
+    GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
 
-        for (int i = 0; i < frameHeight; i++) {
-            for (int j = 0; j < frameWidth; j++) {
-                uint32_t pixel = 0;
-                pixel |= ((int) (j / (float) frameWidth * 255) << 16);
-                pixel |= ((int) (i / (float) frameHeight * 255) << 8);
-                pixel |= 25;
-                frameData[i * frameWidth + j] = pixel;
-            }
+    glfwMakeContextCurrent(window);
+
+    if (gl3wInit()) {
+        std::cerr << "Failed to initialize GL3W\n";
+        return -1;
+    }
+
+    auto program = createShaderProgram("screen.vert", "screen.frag");
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int pbo = createPBO(WindowWidth, WindowHeight);
+    auto texture = createTexture(WindowWidth, WindowHeight);
+
+    unsigned char* data = new unsigned char[WindowWidth * WindowHeight * 3];
+    for (int y = 0; y < WindowHeight; ++y) {
+        for (int x = 0; x < WindowWidth; ++x) {
+            int index = (y * WindowWidth + x) * 3;
+            data[index + 0] = (int)(((float)x / (float)WindowWidth) * 255);
+            data[index + 1] = (int)(((float)y / (float)WindowHeight) * 255);
+            data[index + 2] = 50;
         }
-
-        InvalidateRect(window_handle, nullptr, FALSE);
-        UpdateWindow(window_handle);
     }
 
-    return 0;
-}
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-    case WM_QUIT:
-    case WM_DESTROY: {
-        quit = true;
-    } break;
+        updateTexture(texture, pbo, WindowWidth, WindowHeight, data);
 
-    case WM_PAINT: {
-        static PAINTSTRUCT paint;
-        static HDC device_context;
-        device_context = BeginPaint(hwnd, &paint);
-        BitBlt(device_context,
-               paint.rcPaint.left,
-               paint.rcPaint.top,
-               paint.rcPaint.right - paint.rcPaint.left,
-               paint.rcPaint.bottom - paint.rcPaint.top,
-               frameDeviceContext,
-               paint.rcPaint.left,
-               paint.rcPaint.top,
-               SRCCOPY);
-        EndPaint(hwnd, &paint);
-    } break;
+        glUseProgram(program);
 
-    case WM_SIZE: {
-        bitmapInfo.bmiHeader.biWidth  = LOWORD(lParam);
-        bitmapInfo.bmiHeader.biHeight = HIWORD(lParam);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
 
-        if (bitmap) {
-            DeleteObject(bitmap);
-        }
-        bitmap = CreateDIBSection(nullptr, &bitmapInfo, DIB_RGB_COLORS, (void**) &frameData, 0, 0);
-        SelectObject(frameDeviceContext, bitmap);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        frameWidth  = LOWORD(lParam);
-        frameHeight = HIWORD(lParam);
-    } break;
-
-    default: {
-        return DefWindowProc(hwnd, msg, wParam, lParam);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(program);
+    glDeleteTextures(1, &texture);
+    glfwTerminate();
+
     return 0;
 }
