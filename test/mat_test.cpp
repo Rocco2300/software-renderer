31f9bd90a543bf9e93a53ca45dd4349e3c12248d
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2024-12-19 10:45:54 +0200

    [#3] complete mat testing

--- test/mat_test.cpp
+++ test/mat_test.cpp
@@ -3,7 +3,7 @@
 #include "math/mat.hpp"
 #include "math/vec.hpp"
 
-TEST_CASE("mat addition", "[math]") {
+TEST_CASE("mat3 addition", "[math]") {
     mat3 a(1.f);
     mat3 b(2.f);
 
@@ -13,7 +13,7 @@ TEST_CASE("mat addition", "[math]") {
     REQUIRE(res[2][2] - 3.0f < 0.001f);
 }
 
-TEST_CASE("mat subtraction", "[math]") {
+TEST_CASE("mat3 subtraction", "[math]") {
     mat3 a(5.f);
     mat3 b(2.f);
 
@@ -23,7 +23,7 @@ TEST_CASE("mat subtraction", "[math]") {
     REQUIRE(res[2][2] - 3.0f < 0.001f);
 }
 
-TEST_CASE("mat multiplication", "[math]") {
+TEST_CASE("mat3 multiplication", "[math]") {
     mat3 a(1.f);
     mat3 b(2.f);
 
@@ -36,6 +36,8 @@ TEST_CASE("mat multiplication", "[math]") {
     mat3 d(2.f);
     mat3 e(1.f);
 
+    d[2][0] = 1.f;
+    d[2][1] = 1.f;
     e[2][0] = 3.f;
     e[2][1] = 2.f;
 
@@ -43,11 +45,11 @@ TEST_CASE("mat multiplication", "[math]") {
     REQUIRE(res2[0][0] - 2.0f < 0.001f);
     REQUIRE(res2[1][1] - 2.0f < 0.001f);
     REQUIRE(res2[2][2] - 2.0f < 0.001f);
-    REQUIRE(res2[2][0] - 3.0f < 0.001f);
-    REQUIRE(res2[2][1] - 2.0f < 0.001f);
+    REQUIRE(res2[2][0] - 4.0f < 0.001f);
+    REQUIRE(res2[2][1] - 3.0f < 0.001f);
 }
 
-TEST_CASE("vec-mat multiplication", "[math]") {
+TEST_CASE("vec3-mat3 multiplication", "[math]") {
     mat3 t(1.f);
     vec3 v(1.f);
 
@@ -56,5 +58,71 @@ TEST_CASE("vec-mat multiplication", "[math]") {
     auto res = t * v;
 
     vec3 baseline(3.0f, 2.0f, 1.0f);
+    REQUIRE(res == baseline);
+}
+
+TEST_CASE("mat4 addition", "[math]") {
+    mat4 a(1.f);
+    mat4 b(2.f);
+
+    auto res = a + b;
+    REQUIRE(res[0][0] - 3.0f < 0.001f);
+    REQUIRE(res[1][1] - 3.0f < 0.001f);
+    REQUIRE(res[2][2] - 3.0f < 0.001f);
+    REQUIRE(res[3][3] - 3.0f < 0.001f);
+}
+
+TEST_CASE("mat4 subtraction", "[math]") {
+    mat4 a(5.f);
+    mat4 b(2.f);
+
+    auto res = a - b;
+    REQUIRE(res[0][0] - 3.0f < 0.001f);
+    REQUIRE(res[1][1] - 3.0f < 0.001f);
+    REQUIRE(res[2][2] - 3.0f < 0.001f);
+    REQUIRE(res[3][3] - 3.0f < 0.001f);
+}
+
+TEST_CASE("mat4 multiplication", "[math]") {
+    mat4 a(1.f);
+    mat4 b(2.f);
+
+    auto res1 = a * b;
+    REQUIRE(res1[0][0] - 2.0f < 0.001f);
+    REQUIRE(res1[1][1] - 2.0f < 0.001f);
+    REQUIRE(res1[2][2] - 2.0f < 0.001f);
+    REQUIRE(res1[3][3] - 2.0f < 0.001f);
+
+    mat4 c(1.f);
+    mat4 d(2.f);
+    mat4 e(1.f);
+
+    d[3][0] = 1.f;
+    d[3][1] = 1.f;
+    d[3][2] = 1.f;
+    e[3][0] = 3.f;
+    e[3][1] = 2.f;
+    e[3][2] = 4.f;
+
+    auto res2 = c * d * e;
+    REQUIRE(res2[0][0] - 2.0f < 0.001f);
+    REQUIRE(res2[1][1] - 2.0f < 0.001f);
+    REQUIRE(res2[2][2] - 2.0f < 0.001f);
+    REQUIRE(res2[3][3] - 2.0f < 0.001f);
+    REQUIRE(res2[3][0] - 4.0f < 0.001f);
+    REQUIRE(res2[3][1] - 3.0f < 0.001f);
+    REQUIRE(res2[3][2] - 5.0f < 0.001f);
+}
+
+TEST_CASE("vec4-mat4 multiplication", "[math]") {
+    mat4 t(1.f);
+    vec4 v(1.f);
+
+    t[3][0] = 2.0f;
+    t[3][1] = 1.0f;
+    t[3][2] = 3.0f;
+    auto res = t * v;
+
+    vec4 baseline(3.0f, 2.0f, 4.0f, 1.0f);
     REQUIRE(res == baseline);
 }
